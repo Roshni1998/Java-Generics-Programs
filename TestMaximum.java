e384d7b6fa9b4187c06a7105e32e7f9f5a1fbf35
Roshni1998 <roshnimali847@gmail.com>
2021-06-07 18:46:02 +0530

    Added: Java Generics UC-2

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,10 +1,11 @@
 package com.javagenerics;
 /*
- * Find the maximum from the given 3 integers
+ * 1. Find the maximum from the given 3 integers
+ * 2. Find the maximum from the given 3 floats
  */
 
 public class TestMaximum {
-	
+	//Comparison of integer numbers
 	public static void toPrintMaxInteger(int a, int b, int c) {
 		String str1 = Integer.toString(a);
 		String str2 = Integer.toString(b);
@@ -13,8 +14,18 @@ public class TestMaximum {
 		System.out.println(str1.compareTo(str3));
 		System.out.println(str2.compareTo(str3));
 	}
+	//Comparison of floating point numbers
+	public static void toPrintMaxFloat(float a, float b, float c) {
+		String str1 = Float.toString(a);
+		String str2 = Float.toString(b);
+		String str3 = Float.toString(c);
+		System.out.println(str1.compareTo(str2));
+		System.out.println(str1.compareTo(str3));
+		System.out.println(str2.compareTo(str3));
+	}
 	
 	public static void main(String[] args) {
 		toPrintMaxInteger(34, 47, 34);
+		toPrintMaxFloat(3*10^6, 7*10^6, 5*10^3);
 	}
 }
